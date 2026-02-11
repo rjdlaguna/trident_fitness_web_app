4028e55b30c687119e97d06be2926de86668d7cd
Rene Laguna Jr <rjdlaguna@gmail.com>
2026-02-11 14:14:47 +0800

    Initial landing page design

---
+++ src/assets/Banner.jsx
@@ -0,0 +1,17 @@
+import '../assets/Banner.css'
+export default function Banner() {
+    return(
+        <>
+            <div className='banner-container'>
+                <figure>
+                    <img src ="https://www.sunlife.com.ph/content/dam/sunlife/regional/philippines/images/guide-to-your-first-gym-workout.png"/>
+                </figure>
+                <div className='banner-text'>
+                    <p>TRAIN HARD</p>
+                    <p>LIVE STRONG</p>
+                </div>
+            </div>
+            
+        </>
+    )
+}
---
+++ src/assets/Home/Home.jsx
@@ -0,0 +1,10 @@
+import Header from "../Header"
+import Banner from "../Banner"
+export default function Home() {
+    return(
+        <>
+            <Header />
+            <Banner />
+        </>
+    )
+}
