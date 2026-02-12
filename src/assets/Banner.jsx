41bfc3a28957386729ec026808d389a79c4e21f5
Rene Laguna Jr <rjdlaguna@gmail.com>
2026-02-12 23:32:51 +0800

    Add image slider on landing page

--- src/assets/Banner.jsx
+++ src/assets/Banner.jsx
@@ -1,14 +1,20 @@
 import '../assets/Banner.css'
+import Slider from './Slider.jsx'
 export default function Banner() {
     return(
         <>
             <div className='banner-container'>
-                <figure>
-                    <img src ="https://www.sunlife.com.ph/content/dam/sunlife/regional/philippines/images/guide-to-your-first-gym-workout.png"/>
-                </figure>
-                <div className='banner-text'>
-                    <p>TRAIN HARD</p>
-                    <p>LIVE STRONG</p>
+                <div className=''>
+                    <Slider />
+                </div>
+                <div className='banner-heading'>
+                    <p>TRAIN HARD.</p>
+                    <p>LIVE STRONG.</p>
+                </div>
+                <div className='banner-text'>   
+                    <p>At Trident Fitness, we believe fitness is more than a routine—it’s a lifestyle rooted in strength, confidence, and long‑term well‑being. Our mission is to help people of all ages and fitness levels reach their health goals through expert coaching, science‑based training, and a supportive, motivating community. 
+                    </p>
+                    <button>BEGIN YOUR JOURNEY</button>
                 </div>
             </div>
             
---
+++ src/assets/Slider.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import './Slider.css';
+import { imagesData } from './data/bannerImageData.js'
+
+export default function Slider(){
+
+  const [current, setCurrent] = useState(0);
+  const length = imagesData.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  useEffect(() => {
+    const slideInterval = setInterval(nextSlide, 4000); // Change image every 3 seconds
+    return () => clearInterval(slideInterval); // Cleanup interval on component unmount
+  }, [current, length]);
+
+  if (!Array.isArray(imagesData) || length <= 0) {
+    return null;
+  };
+
+  return(
+    <div className="slider-container">
+      <button onClick={prevSlide} className="arrow prev">&#10094;</button>
+      
+      <button onClick={nextSlide} className="arrow next">&#10095;</button>
+      
+      {imagesData.map((slide, index) => (
+        <div
+          className={index === current ? 'slide active' : 'slide'}
+          key={index}
+        >
+          {index === current && (
+            <img src={slide.imageLocation} alt={slide.imageName} className="image" />
+          )}
+        </div>
+      ))}
+      
+      <div className="dots-container">
+        {imagesData.map((_, index) => (
+          <span
+            key={index}
+            className={index === current ? 'dot active-dot' : 'dot'}
+            onClick={() => setCurrent(index)}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+}
