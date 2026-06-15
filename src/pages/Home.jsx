869a5e6596b9c36db61d7f93312866eeb1e855db
Rene Laguna Jr <rjdlaguna@gmail.com>
2026-06-15 13:08:20 +0800

    Add Footer in the landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar"
 import Banner from "./Banner"
+import Footer from "../components/ui/Footer"
 import TrainersSection from "./TrainersSection"
 import WorkoutSection from "./WorkoutSection"
 import ContactSection from "./ContactSection"
@@ -13,6 +14,7 @@ export default function Home() {
             <WorkoutSection />
             <ContactSection />
             <AboutSection />
+            <Footer />
         </>
     )
 }
